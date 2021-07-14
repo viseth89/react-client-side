8088362b6c3b28933c0fe819fe333e939969d123
viseth89 <viseth89@gmail.com>
2021-07-14 07:42:33 -0500

    Completed 10.3

--- workoutlog2/src/App.js
+++ workoutlog2/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import SiteBar from './home/Navbar'
+import Auth from './auth/Auth'
+import Sitebar from './home/Navbar';
 
 function App() {
+
+  const [sessionToken, setSessionToken] = useState('');
+
+  useEffect(() => {
+    if(localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'))
+    }
+  }, [])
+
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  }
+  // Render method is below
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Sitebar />
+      <Auth />
     </div>
   );
 }
---
+++ workoutlog2/src/auth/Auth.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Signup from './Signup'
+import Login from './Login'
+
+import { Container, Row, Col }  from 'reactstrap';
+
+const Auth = (props) => {
+    return(
+        <Container className="auth-container">
+            <Row>
+                <Col md="6">
+                    Signup
+                    <Signup />
+                </Col>
+                <Col md="6" className="login-col">
+                    Login
+                    <Login />
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Auth;
